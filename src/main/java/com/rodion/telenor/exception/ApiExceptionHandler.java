0a8c117b87e97fba44f21eb0f6691bfb960d56c6
Rodion Zlobin <rodion.zlobin@so4it.com>
2020-05-20 07:49:28 +0200

    added wrong attribute exception handling

--- src/main/java/com/rodion/telenor/exception/ApiExceptionHandler.java
+++ src/main/java/com/rodion/telenor/exception/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.rodion.telenor.exception;
 
 import com.rodion.telenor.domain.ApiError;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,10 +9,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
-    
+
     @ExceptionHandler(EmptyParameterException.class)
     public ResponseEntity<ApiError> emptyParameterException(EmptyParameterException e) {
         String message = e.getMessage() == null ? "Empty request parameters" : e.getMessage();
+        ApiError apiError = ApiError.newBuilder().withMessage(message).build();
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidDataAccessApiUsageException.class)
+    public ResponseEntity<ApiError> wrongParameterException(InvalidDataAccessApiUsageException e) {
+        String message = e.getMessage() == null ? "Wrong request parameter value" : e.getMessage();
         ApiError apiError = ApiError.newBuilder().withMessage(message).build();
         return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
